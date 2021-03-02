565dc8a5d41ec34b0ae25846087871158356f59c
MariA060497 <79877775+MariA060497@users.noreply.github.com>
2021-03-01 22:58:40 -0300

    primeira atividade swift

---
+++ main.swift
@@ -0,0 +1,35 @@
+//1
+var numero: Int
+numero = 5
+
+var total: Int = 6
+
+var valor = 7
+
+//2
+var nomeCarro: String = "carro"
+var codigoCarro: Int = 9999999
+var preco: Double = 222.33
+var clienteNome: String = "Julio"
+var enderecoCliebte = "Rua x"
+
+//3
+
+//Anotação de tipos no Swift e o tipo que a variavel recebe seja ele string, int, etc.
+
+//4
+
+var int: Int = 2
+var double: Double = 2.2
+var text = String(double) + " " + String(int)
+print (text)
+
+//5
+
+//safety
+
+//E a linguagem que te incentiva a ser claro quanto ao tipo da variavel.
+
+//inference
+
+//E quando não e especificado o tipo da variavel e a linguagem deduz o tipo de variavel com base no valor aderido.
